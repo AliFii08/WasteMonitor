669199ce516c5f2d6732d516200c837a7e1ea3ff
Ali <a.figueroa@urbe.edu.ve>
2026-02-13 10:45:04 -0400

    nuevosCambiosRegistros

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,5 +9,16 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './register.scss',
 })
 export class Register {
-
+  registerForm = new FormGroup({
+    name: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    lastName: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    email: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.email] }),
+    phone: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    password: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    confirmPassword: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    sector: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    street: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    houseNumber: new FormControl('', { nonNullable: true }),
+    postalCode: new FormControl('', { nonNullable: true }),
+  });
 }
