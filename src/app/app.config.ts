f8ce0df441fa0bfb8093dfb2e93a16e6ef1296e8
Ali <aliothfigueroa.08@gmail.com>
2026-02-24 15:21:51 -0400

    Cambios Registro y Login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,11 +7,16 @@ import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth } from '@angular/fire/auth';
+import { provideDatabase } from '@angular/fire/database';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 import { environment } from '../environments/environment';
 import { MessageService } from 'primeng/api';
+
+const firebaseApp = initializeApp(environment.firebaseConfig);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -25,8 +30,8 @@ export const appConfig: ApplicationConfig = {
       },
     }),
     provideClientHydration(withEventReplay()),
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideAuth(() => getAuth()),
-    provideDatabase(() => getDatabase()),
+    provideFirebaseApp(() => firebaseApp),
+    provideAuth(() => getAuth(firebaseApp)),
+    provideDatabase(() => getDatabase(firebaseApp)),
   ],
 };
