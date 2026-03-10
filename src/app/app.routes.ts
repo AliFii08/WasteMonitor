bdcfece14293973b7cca5ca249ab8f069351df75
Ali <aliothfigueroa.08@gmail.com>
2026-03-09 22:45:20 -0400

    arreglo en rutas y prueba de jwtGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Layout } from './@core/layout/layout';
+import { jwtGuard } from './@core/guards/jwt.guard';
 //import { jwtGuard } from './@core/guards/jwt.guard';
 
 export const routes: Routes = [
@@ -10,19 +11,13 @@ export const routes: Routes = [
     canActivate: [/*jwtGuard*/],
     children: [
       {
-        path: '',
-        title: 'Inicio',
-        loadComponent: () => 
-          import('./pages/home/home').then(m => m.Home),
-      },
-      {
         path: 'home',
         title: 'Inicio',
-        loadComponent: () => 
+        loadComponent: () =>
           import('./pages/home/home').then(m => m.Home),
       },
       {
-        path: 'home/horarios',
+        path: 'home/schedules',
         title: 'Horarios',
         loadComponent: () =>
           import('./pages/schedules/schedules').then(m => m.Schedules),
@@ -54,6 +49,12 @@ export const routes: Routes = [
     ]
   },
   {
+    path: 'login',
+    title: 'Iniciar Sesión',
+    loadComponent: () =>
+      import('./@core/auth/login/login').then(m => m.Login),
+  },
+  {
     path: 'register',
     title: 'Registrarse',
     loadComponent: () =>
@@ -64,12 +65,6 @@ export const routes: Routes = [
     title: 'Cambiar Contraseña',
     loadComponent: () =>
       import('./@core/auth/change-password/change-password').then(m => m.ChangePassword),
-  },
-  {
-    path: 'login',
-    title: 'Iniciar Sesión',
-    loadComponent: () =>
-      import('./@core/auth/login/login').then(m => m.Login),
   },
   {
     path: '**',
