adbb53b5c9f889b907e62920c3dc8b5e080bd17a
Manudev01 <bymadby80@gmail.com>
2026-08-12 16:48:14 -0400

    Adicion de obtencion de empleados y conductores

--- src/app/@core/services/conductores.service.ts
+++ src/app/@core/services/conductores.service.ts
@@ -1,1 +1,43 @@
-// Agregar codigo
+import { inject, Injectable } from '@angular/core';
+import { Database, ref, get, query, orderByChild, equalTo } from '@angular/fire/database';
+import { FirebaseUser } from '../interfaces/user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConductoresService {
+  private database = inject(Database);
+
+  /**
+   * Obtiene todos los usuarios y los filtra localmente por rol 'conductor'
+   */
+  async getConductores(): Promise<FirebaseUser[]> {
+    try {
+      const usersRef = ref(this.database, 'usuarios');
+      const snapshot = await get(usersRef);
+
+      const conductores: FirebaseUser[] = [];
+
+      if (snapshot.exists()) {
+        snapshot.forEach((childSnapshot) => {
+          const userData = childSnapshot.val();
+
+          // Filtrado directo en JavaScript
+          if (userData && userData.rol === 'conductor') {
+            conductores.push({
+              uid: childSnapshot.key,
+              ...userData,
+            });
+          }
+        });
+      }
+
+      console.log('Lista de conductores:', conductores);
+      return conductores;
+
+    } catch (error) {
+      console.error('Error al obtener la lista de conductores:', error);
+      return [];
+    }
+  }
+}
--- src/app/pages/drivers/drivers.ts
+++ src/app/pages/drivers/drivers.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import { ConductoresService } from '../../@core/services/conductores.service';
+import { EmpleadoService } from '../../@core/services/empleado.service';
 @Component({
   selector: 'app-drivers',
   imports: [],
   templateUrl: './drivers.html',
   styleUrl: './drivers.scss',
 })
-export class Drivers {
+export class Drivers implements OnInit {
 
+  private conductoresService = inject(ConductoresService);
+  private empleadoService = inject(EmpleadoService);
+
+  async ngOnInit() {
+    await this.conductoresService.getConductores();
+    await this.empleadoService.getEmpleado();
+  }
+
+  
 }
