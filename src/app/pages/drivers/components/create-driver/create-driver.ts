cf2002bc33990de91dbe9ee33fc7b3bed5ee031d
Manudev01 <bymadby80@gmail.com>
2026-09-12 18:40:26 -0400

    Iniciacion de componentes de drivers

---
+++ src/app/pages/drivers/components/create-driver/create-driver.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-driver',
+  imports: [],
+  templateUrl: './create-driver.html',
+  styleUrl: './create-driver.scss',
+})
+export class CreateDriver {
+
+}
---
+++ src/app/pages/drivers/components/update-driver/update-driver.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-driver',
+  imports: [],
+  templateUrl: './update-driver.html',
+  styleUrl: './update-driver.scss',
+})
+export class UpdateDriver {
+
+}
